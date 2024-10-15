3f9c3ab17ba345c32b27b1216f48ef57a42191e0
TANISHQDAS <tanishqdas2005@gmail.com>
2024-10-15 18:24:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("option1").addEventListener("click", function() {
+    window.location.href = 'plant-mushroom-identify.html';
+});
+
+document.getElementById("option2").addEventListener("click", function() {
+    window.location.href = '#'; // Link to plant disease identification page
+});
+
+document.getElementById("option3").addEventListener("click", function() {
+    alert("Under Development");
+});
+
+document.getElementById("option4").addEventListener("click", function() {
+    alert("Under Development");
+});
